9a5d55250f05ce9e8968203420228eefe2e2e5fa
X8J <37266311+X8J@users.noreply.github.com>
2024-02-27 19:36:26 -0500

    Update betterAdjustmentLayers.jsx

--- betterAdjustmentLayers.jsx
+++ betterAdjustmentLayers.jsx
@@ -1,46 +1,36 @@
-
 function renameAdjustmentLayers() {
     var comp = app.project.activeItem;
-    
-    
     if (comp && comp instanceof CompItem) {
         var layers = comp.layers;
-        
-       
         for (var i = 1; i <= layers.length; i++) {
             var layer = layers[i];
-         
             if (layer instanceof AVLayer && layer.adjustmentLayer) {
                 var effects = layer.property("ADBE Effect Parade");
-                
-                // check if adj layer has effects applied
                 if (effects.numProperties > 0) {
                     var newName = "";
-                    
-                    //look thorugh effects applied on adusmtnet layer
                     for (var j = 1; j <= effects.numProperties; j++) {
                         var effect = effects.property(j);
-                        
-                        
                         var effectName = effect.name;
-                        
-                        
-                        newName +=  effectName + "+";
+                        newName += effectName + "+";
                     }
-                    
-                    //set name
                     layer.name = newName;
                 }
             }
         }
-        
-       
         alert("Renamed");
     } else {
-       
-        alert("No composition found");
+        alert("Composition not found");
     }
 }
 
-// Call the function to rename adjustment layers
-renameAdjustmentLayers();
+function createGUI() {
+    var window = new Window("palette", "Kai BetterAdjLayers");
+    window.orientation = "column";
+    var button = window.add("button", undefined, "Rename Layers");
+    button.onClick = function() {
+        renameAdjustmentLayers();
+    };
+    window.show();
+}
+
+createGUI();
