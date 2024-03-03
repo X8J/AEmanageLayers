2b4c8a9841242a2cc00a86e3ae458d9584a8bc7f
X8J <37266311+X8J@users.noreply.github.com>
2024-03-03 17:48:27 -0500

    Update betterAdjLayersMANUAL.jsx
    Added fixes 
    Inconsistency
    Breaking in precomps

--- betterAdjLayersMANUAL.jsx
+++ betterAdjLayersMANUAL.jsx
@@ -1,25 +1,24 @@
 function renameAdjustmentLayers() {
     var comp = app.project.activeItem;
-    if (comp && comp instanceof CompItem) {
-        var layers = comp.layers;
-        for (var i = 1; i <= layers.length; i++) {
-            var layer = layers[i];
-            if (layer instanceof AVLayer && layer.adjustmentLayer) {
-                var effects = layer.property("ADBE Effect Parade");
-                if (effects.numProperties > 0) {
-                    var newName = "";
-                    for (var j = 1; j <= effects.numProperties; j++) {
-                        var effect = effects.property(j);
-                        var effectName = effect.name;
-                        newName +=  effectName + " ";
-                    }
-                    layer.name = newName;
+    if (!comp || !(comp instanceof CompItem)) {
+        alert("Please select a composition.");
+        return;
+    }
+
+    var layers = comp.layers;
+    for (var i = 1; i <= layers.length; i++) {
+        var layer = layers[i];
+        if (layer instanceof AVLayer && layer.adjustmentLayer) {
+            var effects = layer.property("ADBE Effect Parade");
+            if (effects.numProperties > 0) {
+                var newName = "";
+                for (var j = 1; j <= effects.numProperties; j++) {
+                    var effect = effects.property(j);
+                    newName += effect.name + " ";
                 }
+                layer.name = newName;
             }
         }
-        
-    } else {
-        alert("Please select a composition.");
     }
 }
 
@@ -30,15 +29,14 @@ function createDockableUI(thisObj) {
             : new Window("palette", undefined, undefined, { resizeable: true });
     dialog.alignChildren = "center";
 
-   
-    
-
     var button = dialog.add("button", undefined, "Rename Layers");
     button.alignment = ["fill", "top"];
-
     button.onClick = function() {
         renameAdjustmentLayers();
     };
+
+    var discordText = dialog.add("statictext", undefined, "Discord: c.op");
+    var githubText = dialog.add("statictext", undefined, "Github.com/x8j");
 
     dialog.onResizing = dialog.onResize = function() {
         this.layout.resize();
@@ -52,8 +50,7 @@ function showWindow(myWindow) {
     if (myWindow instanceof Window) {
         myWindow.center();
         myWindow.show();
-    }
-    if (myWindow instanceof Panel) {
+    } else if (myWindow instanceof Panel) {
         myWindow.layout.layout(true);
         myWindow.layout.resize();
     }
